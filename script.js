64d1887119ee662c47fd453bf631506d2e23f3ef
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-07-26 14:28:15 +0300

    add book constructor

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(name, author, year, description, cover) {
+  this.name = name;
+  this.author = author;
+  this.year = year;
+  this.description = description;
+  this.cover = cover;
+  
 }
 
 function addBookToLibrary() {

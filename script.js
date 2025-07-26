f6aeaee66604beda7b8241183016cdf3bfd3a1b4
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-07-26 19:47:39 +0300

    add modal with book info
    - add morestyles
    - add verification for books that are already on the page

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const myLibrary = [
   {
     id: '100173ea-fd9c-44fb-ae46-922a1366eb02',
-    name: 'Book',
-    author: 'Ben',
-    year: 1999,
-    description: 'description',
-    cover: 'cover'
+    name: 'The Way of Kings',
+    author: 'Brandon Sanderson',
+    year: 2010,
+    description: 'The Stormlight Archive is an epic fantasy series by Brandon Sanderson. The series is expected to be composed of ten books, in keeping with the series\' symmetrical focus in combinations of ten.However, those ten books will be divided into two groups of five.',
+    cover: 'https://miro.medium.com/v2/resize:fit:4800/format:webp/1*QD5qZosh_aE2VPVSxGYmxQ.jpeg'
   }
 ];
 
@@ -22,16 +22,62 @@ function addBookToLibrary(name, author, year, description, cover) {
   myLibrary.push(new Book(name, author, year, description, cover));
 }
 
-function addBookToList() {
+function addBookToPage() {
 
   myLibrary.forEach((book) => {
-    const article = document.createElement("article");
-    article.textContent = `
-    Назва: ${book.name},
-    Автор: ${book.author}`
-    body.appendChild(article);
+
+    const allCards = document.querySelectorAll(".card");
+    let alreadyExists = false;
+    allCards.forEach((bookOnPage) => {
+      if (bookOnPage.id === book.id) {
+        alreadyExists = true;
+      }
+    });
+    if (alreadyExists) return;
+
+    const newBook = document.createElement("div");
+    newBook.className = "card";
+    const bookName = document.createElement("div");
+    bookName.className = "desc";
+    bookName.textContent = book.name;
+    const bookCover = document.createElement("div");
+    bookCover.className = "img-block";
+    bookCover.style.background = `url(${book.cover}) 358px`;
+    bookCover.style.backgroundSize = `358px`;
+
+    newBook.id = book.id;
+
+    newBook.appendChild(bookCover);
+    newBook.appendChild(bookName);
+
+    const firstChild = bookTable.firstChild;
+    bookTable.insertBefore(newBook, firstChild);
   });
 }
 
 const addButton = document.querySelector(".add-btn");
 const addCards = document.querySelector(".add-cards");
+const bookTable = document.querySelector(".book-conteiner");
+const dialog = document.querySelector('dialog');
+
+addBookToPage();
+
+bookTable.addEventListener('click', (e) => {
+  const book = e.target.closest('.card');
+  if (!book) return;
+
+  const found = myLibrary.find(item => item.id === book.id);
+
+  if (found) {
+    const modalHeader = document.querySelector(".modal-header");
+    modalHeader.textContent = found.name;
+
+    const modalCover = document.querySelector(".modal-img-block")
+    modalCover.style.background = `url(${found.cover}) center`;
+    modalCover.style.backgroundSize = `975px`;
+
+
+
+    dialog.showModal();
+  }
+});
